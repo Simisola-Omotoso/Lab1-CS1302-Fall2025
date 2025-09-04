0c8fe30239025d6e72687d68bcb5ae61fef97a34
Simi0M0 <somotoso@Outlook.com>
2025-09-04 07:04:08 -0400

    Added testOneItemGetText and Modified Former Test Case
    1) Added new test case for testing a bill formatter that has only one item and renamed the other one as empty
    2) To test one item on the bill and make the test cases clearer
    3) Duplicated previous test case, renamed it to an empty test case, and modified the previous one by adding a new BillItem, used the addItem method, and renamed the previous one testOneItemGetText

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/view/bill_view/TestGetText.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/view/bill_view/TestGetText.java
@@ -5,13 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import edu.westga.cs1302.lab2.model.Bill;
-
+import edu.westga.cs1302.lab2.model.BillItem;
 import edu.westga.cs1302.lab2.view.BillView;
 
 class TestGetText {
 	
 	@Test
-	public void testNormalGetText() {
+	public void testOneItemGetText() {
+		Bill bill = new Bill();
+		BillItem item = new BillItem("Tablet", 109.99);
+		bill.addItem(item);
+		BillView view = new BillView();
+		String result = view.getText(bill);
+		assertEquals(view.getText(bill), result);
+	}
+	
+	@Test
+	public void testEmptyGetText() {
 		Bill bill = new Bill();
 		BillView view = new BillView();
 		String result = view.getText(bill);
