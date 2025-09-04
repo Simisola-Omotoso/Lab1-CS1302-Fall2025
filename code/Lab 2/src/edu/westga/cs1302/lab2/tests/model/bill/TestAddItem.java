4604339c4e9e55148c0a75c75193f336c42bd720
Simi0M0 <somotoso@Outlook.com>
2025-09-04 05:48:41 -0400

    Added testIllegalArgumentExceptionAddItem
    1) Added a test for illegal argument in addItem
    2) To test if illegal argument works
    3) Wrote new test, created new bill, and wrote assertThrows for a null BillItem

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill/TestAddItem.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill/TestAddItem.java
@@ -1,5 +1,16 @@
 package edu.westga.cs1302.lab2.tests.model.bill;
 
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab2.model.BillItem;
+
+import org.junit.jupiter.api.Assertions.*;
+
 public class TestAddItem {
+	
+	@Test
+	public void testIllegalArgumentExceptionAddItem() {
+		BillItem item = new BillItem();
+	}
 
 }
