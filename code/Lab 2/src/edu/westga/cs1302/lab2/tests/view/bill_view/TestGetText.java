b17076af0d00ce970afc649dc1f850c8ce647d1b
Simi0M0 <somotoso@Outlook.com>
2025-09-04 06:21:13 -0400

    Added testNormalGetText
    1) Added new test case for getText method
    2) To test sunny day method
    3) Created new test with new bill, new billview, and added assertEquals

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/view/bill_view/TestGetText.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/view/bill_view/TestGetText.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.lab2.model.Bill;
+
+import edu.westga.cs1302.lab2.view.BillView;
+
 class TestGetText {
 	
 	@Test
-	void test() {
-		fail("Not implemented yet");
+	public void testNormalGetText() {
+		Bill bill = new Bill();
+		BillView view = new BillView();
+		String result = view.getText(bill);
+		assertEquals(view.getText(bill), result);
 	}
 
 }
